70b66fd8d087e3e1c0f6bebd2ef8eda3f029d6b3
theodorerlax <sampjohn@mail.ru>
2021-10-13 17:35:38 +0300

    add class Role

---
+++ src/main/java/com/example/demo/model/Role.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role extends BaseEntity{
+
+    @Column(name = "name")
+    private String name;
+}
