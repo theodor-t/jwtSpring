f613db828ca4db324070cd0fd809d0b9e56c6b95
theodorerlax <sampjohn@mail.ru>
2021-10-13 17:29:51 +0300

    add anotations

--- src/main/java/com/example/demo/model/BaseEntity.java
+++ src/main/java/com/example/demo/model/BaseEntity.java
@@ -1,15 +1,28 @@
 package com.example.demo.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.util.Date;
 
 @MappedSuperclass
 @Data
 public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @CreatedDate
+    @Column(name = "created")
     private Date created;
+
+    @LastModifiedDate
+    @Column(name = "updated")
     private Date updated;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
     private Status status;
 }
