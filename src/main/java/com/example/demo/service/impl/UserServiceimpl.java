3bb9dded9cba7ee00e42a119983b2f4a5e783d7a
theodorerlax <sampjohn@mail.ru>
2021-10-16 19:23:24 +0300

    create: + UserServiceimpl + JwtUser + JwtUserDetailsService + JwtUserFactory

--- src/main/java/com/example/demo/service/impl/UserServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceimpl.java
@@ -7,6 +7,7 @@ import com.example.demo.repository.RoleRepository;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
