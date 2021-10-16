362876fe426f43488d9274c27c108f3e1c920e5d
theodorerlax <sampjohn@mail.ru>
2021-10-16 20:08:10 +0300

    fix import

--- src/main/java/com/example/demo/rest/AuthenticationRestControllerV1.java
+++ src/main/java/com/example/demo/rest/AuthenticationRestControllerV1.java
@@ -1,5 +1,6 @@
 package com.example.demo.rest;
 
+import com.example.demo.dto.AuthenticationRequestDto;
 import com.example.demo.model.User;
 import com.example.demo.security.jwt.JwtTokenProvider;
 import com.example.demo.service.UserService;
@@ -8,13 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.naming.AuthenticationException;
+
 import java.util.HashMap;
 import java.util.Map;
 
